c39a43dc1fd32dcad8ea65f27396e4777ec41a3d
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-09-03 06:03:23 -0500

    u

--- lua/plugins/do-the-needful.lua
+++ lua/plugins/do-the-needful.lua
@@ -1,7 +1,7 @@
 local dev = false
 local e = vim.tbl_extend
 local m = require("util").lazy_map
-vim.g.tmux_tasks_log_level = "debug"
+vim.g_do_the_needful_log_level = "debug"
 
 local opts = {
 	tasks = {},
