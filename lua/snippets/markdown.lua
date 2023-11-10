4a8af22c324d09ec3f3b983a8faa3aaacbb6daf4
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2023-11-10 05:45:36 -0600

    Added table generator function to markdown snippets

--- lua/snippets/markdown.lua
+++ lua/snippets/markdown.lua
@@ -1,6 +1,13 @@
 local ls = require("luasnip")
-local s, t, i, c, r, f, sn =
-	ls.snippet, ls.text_node, ls.insert_node, ls.choice_node, ls.restore_node, ls.function_node, ls.snippet_node
+local s, t, i, c, r, f, sn, d =
+	ls.snippet,
+	ls.text_node,
+	ls.insert_node,
+	ls.choice_node,
+	ls.restore_node,
+	ls.function_node,
+	ls.snippet_node,
+	ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local u = require("util.luasnip")
@@ -18,6 +25,34 @@ local snippets = {
 			{ i(1) }
 		)
 	),
+	s({ trig = "table(%d+)x(%d+)", regTrig = true }, {
+		d(1, function(args, snip)
+			local nodes = {}
+			local i_counter = 0
+			local hlines = ""
+			for _ = 1, snip.captures[2] do
+				i_counter = i_counter + 1
+				table.insert(nodes, t("| "))
+				table.insert(nodes, i(i_counter, "Column" .. i_counter))
+				table.insert(nodes, t(" "))
+				hlines = hlines .. "|---"
+			end
+			table.insert(nodes, t({ "|", "" }))
+			hlines = hlines .. "|"
+			table.insert(nodes, t({ hlines, "" }))
+			for _ = 1, snip.captures[1] do
+				for _ = 1, snip.captures[2] do
+					i_counter = i_counter + 1
+					table.insert(nodes, t("| "))
+					table.insert(nodes, i(i_counter))
+					print(i_counter)
+					table.insert(nodes, t(" "))
+				end
+				table.insert(nodes, t({ "|", "" }))
+			end
+			return sn(nil, nodes)
+		end),
+	}),
 }
 
 return snippets
