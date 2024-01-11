0205794803db55a01f87941bac0156c3f990ba2d
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-01-11 09:19:24 -0600

    Remove disabled image integration plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -69,24 +69,6 @@ return {
 		"ariel-frischer/bmessages.nvim",
 		event = "CmdlineEnter",
 		config = true,
-	},
-	{
-		"3rd/image.nvim",
-		event = "VeryLazy",
-		enabled = false,
-		opts = {
-			-- backend = "kitty",
-			tmux_show_only_in_active_window = true,
-			-- kitty_method = "normal",
-			integrations = {
-				markdown = {
-					enabled = true,
-					clear_in_insert_mode = false,
-					download_remote_images = true,
-					only_render_image_at_cursor = false,
-					filetypes = { "markdown", "vimwiki" },
-				},
-			},
-		},
+		enabled = true,
 	},
 }
