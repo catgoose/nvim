952ae69da6a74a9d785e14a16108ba97fed8cb31
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-04-07 06:07:32 -0500

    Refactored project detection with additional Vue 3 configuration support.

--- lua/plugins/vue-goto-definition.lua
+++ lua/plugins/vue-goto-definition.lua
@@ -1,4 +1,4 @@
-local dev = true
+local dev = false
 local enabled = true
 local e = vim.tbl_extend
 local project = require("util.project")
@@ -9,8 +9,18 @@ local opts = {
 	filters = {
 		auto_imports = true,
 		auto_components = true,
-		same_file = true,
+		import_same_file = true,
 		declaration = true,
+		duplicate_filename = true,
+	},
+	detection = {
+		nuxt = function()
+			return vim.fn.glob(".nuxt/") ~= ""
+		end,
+		vue3 = function()
+			return vim.fn.filereadable("vite.config.ts") == 1 or vim.fn.filereadable("src/App.vue") == 1
+		end,
+		priority = { "nuxt", "vue3" },
 	},
 	lsp = {
 		override_definition = false,
