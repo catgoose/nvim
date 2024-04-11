a94c457d8115b21b5377560a592ef5cc411e1b51
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-04-11 18:32:25 -0500

    Update list of tools in tool installer Lua script

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -2,14 +2,14 @@ local opts = {
 	auto_update = true,
 	run_on_start = true,
 	ensure_installed = {
-		"prettierd",
-		"stylua",
-		"shellcheck",
-		"misspell",
-		"eslint_d",
 		"beautysh",
-		"jsonlint",
+		"eslint_d",
 		"hadolint",
+		"jsonlint",
+		"misspell",
+		"prettierd",
+		"shellcheck",
+		"stylua",
 	},
 }
 
