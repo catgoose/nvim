a4cbce1eccd37cc5b339be643b08413cb6e8118a
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-04-11 21:02:26 -0500

    Refactored formatters_by_ft table to include additional file types and formatters.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,55 +1,63 @@
 local c = require("util").create_cmd
 
 local prettier = {
-  "prettierd",
-  "prettier",
+	"prettierd",
+	"prettier",
 }
 local opts = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { prettier },
-    typescript = { prettier },
-    vue = { prettier },
-  },
-  format_on_save = function(bufnr)
-    if vim.b[bufnr].disable_autoformat then
-      return
-    end
-    return { timeout_ms = 500, lsp_fallback = true }
-  end,
-  formatters = {
-    shfmt = {
-      prepend_args = { "-i", "2" },
-    },
-  },
+	formatters_by_ft = {
+		css = { prettier },
+		html = { prettier },
+		javascript = { prettier },
+		json = { prettier },
+		jsonc = { prettier },
+		lua = { "stylua" },
+		scss = { prettier },
+		typescript = { prettier },
+		vue = { prettier },
+		fish = { "fish_indent" },
+		sh = { "shfmt" },
+		bash = { "shfmt" },
+	},
+	format_on_save = function(bufnr)
+		if vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 500, lsp_fallback = true }
+	end,
+	formatters = {
+		shfmt = {
+			prepend_args = { "-i", "2" },
+		},
+	},
 }
 
 local function init()
-  local function notify()
-    require("notify").notify(
-      string.format("Auto formatting %s", vim.b.disable_autoformat and "enabled" or "disabled"),
-      vim.log.levels.info,
-      { title = "conform.nvim formatting" }
-    )
-  end
-  c("ConformFormatDisable", function()
-    if not vim.b.disable_autoformat then
-      notify()
-    end
-    vim.b.disable_autoformat = true
-  end)
-  c("ConformFormatEnable", function()
-    if vim.b.disable_autoformat then
-      notify()
-    end
-    vim.b.disable_autoformat = false
-  end)
+	local function notify()
+		require("notify").notify(
+			string.format("Auto formatting %s", vim.b.disable_autoformat and "enabled" or "disabled"),
+			vim.log.levels.info,
+			{ title = "conform.nvim formatting" }
+		)
+	end
+	c("ConformFormatDisable", function()
+		if not vim.b.disable_autoformat then
+			notify()
+		end
+		vim.b.disable_autoformat = true
+	end)
+	c("ConformFormatEnable", function()
+		if vim.b.disable_autoformat then
+			notify()
+		end
+		vim.b.disable_autoformat = false
+	end)
 end
 
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  opts = opts,
-  cmd = { "ConformInfo" },
-  init = init,
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	opts = opts,
+	cmd = { "ConformInfo" },
+	init = init,
 }
---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,36 @@
+local linters = {
+	js = { "eslint_d" },
+	sh = { "shellharden", "shellcheck" },
+	all = { "codespell" },
+}
+
+return {
+	"mfussenegger/nvim-lint",
+	event = "BufReadPre",
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			javascript = linters.js,
+			typescript = linters.js,
+			vue = linters.js,
+			docker = { "hadolint" },
+			fish = { "fish" },
+			env = { "dotenv-linter" },
+			json = { "jsonlint" },
+			markdown = { "markdownlint", "vale", "write-good", "cbfmt" },
+			editorconfig = { "editorconfig-checker" },
+			sh = linters.sh,
+			bash = linters.sh,
+		}
+
+		lint.linters.codespell.args = { "--ignore-words=~/.config/codespell/ignore_word" }
+
+		vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
+			group = vim.api.nvim_create_augroup("lint", { clear = true }),
+			callback = function()
+				lint.try_lint()
+				lint.try_lint(linters.all)
+			end,
+		})
+	end,
+}
