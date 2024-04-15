1b172aea8472482237a4f83463e79695724f85a5
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-04-15 11:51:30 -0500

    Add Injectme.nvim plugin with dependencies for lazy loading and performance enhancement.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -61,4 +61,14 @@ return {
 	-- 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	-- 	config = true,
 	-- },
+	{
+		"Dronakurl/injectme.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		-- This is for lazy load and more performance on startup only
+		cmd = { "InjectmeToggle", "InjectmeSave", "InjectmeInfo", "InjectmeLeave" },
+	},
 }
