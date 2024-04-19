bbedd38236fc3bc9a300d26046610158062fe022
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-04-19 07:11:04 -0500

    Added new element and template functions to textobjects file.

--- queries/vue/textobjects.scm
+++ queries/vue/textobjects.scm
@@ -18,3 +18,10 @@
 (template_element
   ( element) @block.inner
   )
+
+
+(element (start_tag (tag_name) @method_parameter) )
+
+(template_element ) @function.outer
+
+(style_element) @this_method_call
