29705483c7ba2170d74015a3e2672d975e263642
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-05-11 15:07:11 -0500

    Updates nvim

--- lua/plugins/ts-node-action.lua
+++ lua/plugins/ts-node-action.lua
@@ -59,12 +59,53 @@ local function class_action(tsnode)
   end
 end
 
+local function get_node_text(tsnode)
+  return vim.treesitter.get_node_text(tsnode, 0)
+end
+
 return {
   "CKolkey/ts-node-action",
   opts = {
     vue = {
       ["attribute_name"] = function(tsnode)
-        class_action(tsnode)
+        -- class_action(tsnode)
+        local node_text = get_node_text(tsnode)
+        if node_text == "class" then
+          local sibling = tsnode:next_named_sibling()
+          if not sibling then
+            return
+          end
+          local classes_text = get_node_text(sibling)
+          if not classes_text then
+            return
+          end
+
+          local quote = classes_text:sub(1, 1)
+
+          local multiline = string.find(classes_text, "\n")
+          local opts = {
+            target = sibling,
+            callback = function()
+              vim.b.disable_autoformat = not vim.b.disable_autoformat and not multiline
+            end,
+          }
+
+          if multiline then
+            local classes =
+              classes_text:gsub("\n", " "):gsub(quote .. "%s+", quote):gsub("%s+" .. quote, quote)
+            return classes, opts
+          else
+            local _classes = u.split_string(classes_text)
+            _classes[1] = _classes[1]:sub(2)
+            _classes[#_classes] = _classes[#_classes]:sub(1, -2)
+            local classes = { quote }
+            for _, class in ipairs(_classes) do
+              table.insert(classes, class)
+            end
+            classes[#classes + 1] = quote
+            return classes, opts
+          end
+        end
       end,
       ["attribute_value"] = function(tsnode)
         tsnode = tsnode:parent()
