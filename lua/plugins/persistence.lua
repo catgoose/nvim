d2c5334f6152c0ed61efe7c7fb3f1e578051879a
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-05-16 13:26:35 -0500

    Update persistence.lua plugin to use fork "folke/persistence.nvim" and trigger event on "BufReadPre".

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -2,10 +2,7 @@ local ufo_u = require("util.ufo")
 local const = require("config.constants").const
 
 return {
-  -- "folke/persistence.nvim",
-  --  NOTE: 2024-04-13 - Using fork to allow for post_load hook until PR is
-  --  merged: https://github.com/folke/persistence.nvim/pull/24
-  "5c077m4n/persistence.nvim",
+  "folke/persistence.nvim",
   event = "BufReadPre",
   opts = {
     dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"),
