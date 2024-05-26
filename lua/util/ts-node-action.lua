6696cd6746bfeab957f277526ce8e3b6c7386d6d
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-05-26 06:41:51 -0500

    Refactored handle_tag_name function to return concatenated element and opts for multiline parent_text.

--- lua/util/ts-node-action.lua
+++ lua/util/ts-node-action.lua
@@ -21,7 +21,6 @@ local function handle_attribute_value(tsnode)
       :gsub(quote .. "%s+", quote)
       :gsub("%s+" .. quote, quote)
       :gsub("%s+", " ")
-    opts.cursor = {}
     return line, opts
   else
     local _attrs = u.split_string(node_text)
@@ -33,7 +32,6 @@ local function handle_attribute_value(tsnode)
     for _, attr in ipairs(_attrs) do
       table.insert(attrs, spaces .. (" "):rep(vim.bo.shiftwidth) .. attr)
     end
-    opts.cursor = { row = 1 }
     attrs[#attrs + 1] = spaces .. quote
     return attrs, opts
   end
@@ -41,32 +39,36 @@ end
 
 local function handle_tag_name(tsnode)
   local tag_text = u.get_node_text(tsnode)
+
   local parent = tsnode:parent()
   if not parent then
     return
   end
   local parent_text = u.get_node_text(parent)
+
   local tag_start = parent_text:sub(1, 1)
   local tag_end = parent_text:sub(-1)
-
-  local tag_tbl = { tag_start .. tag_text }
-
+  local element = { tag_start .. tag_text }
   local sibling = tsnode:next_named_sibling()
   while sibling do
-    table.insert(tag_tbl, (" "):rep(vim.bo.shiftwidth * 2) .. u.get_node_text(sibling))
+    table.insert(element, u.get_node_text(sibling))
     sibling = sibling:next_named_sibling()
   end
-  table.insert(tag_tbl, (" "):rep(vim.bo.shiftwidth) .. tag_end)
+  element[#element + 1] = tag_end
 
+  local multiline = string.find(parent_text, "\n")
   local opts = {
-    cursor = { row = 1, col = vim.bo.shiftwidth },
     target = tsnode:parent(),
     callback = function()
       vim.b.disable_autoformat = true
     end,
     format = true,
   }
-  return tag_tbl, opts
+  if multiline then
+    return table.concat(element, " "), opts
+  else
+    return element, opts
+  end
 end
 
 M.vue = {
@@ -79,8 +81,6 @@ M.vue = {
     if not attribute_value then
       return
     end
-    --  TODO: 2024-05-25 - What does this mean and what should be returned
-    -- if multiple lines aren't received?
     if #u.split_string(attribute_value) > 1 then
       return handle_attribute_value(sibling)
     end
