453793c0c1e0b6b4b93d33c3171215017898dc24
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-06-02 07:21:20 -0500

    Refactor neotest setup in init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,28 +31,4 @@ return {
     "folke/neoconf.nvim",
     lazy = true,
   },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "marilari88/neotest-vitest",
-      "thenbe/neotest-playwright",
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-vitest"),
-          require("neotest-playwright").adapter({
-            options = {
-              persist_project_selection = true,
-              enable_dynamic_test_discovery = true,
-            },
-          }),
-        },
-      })
-    end,
-  },
 }
