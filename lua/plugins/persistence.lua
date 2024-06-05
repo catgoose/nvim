2934610256d5065bad3597c3942a642536bbb7c0
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-06-04 20:43:01 -0500

    fix: adjust lua/plugins/persistence.lua to include necessary imports

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,9 +1,6 @@
 local ufo_u = require("util.ufo")
 local const = require("config.constants").const
 
---  TODO: 2024-06-02 -  close windows with a table of filetypes, e.g.
---  {"neotest-output-panel"}
-
 return {
   "folke/persistence.nvim",
   event = "BufReadPre",
