f3b4fecb3ce6cfb632efe2323af7f02344e077da
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-06-28 06:15:18 -0500

    fix: remove unnecessary plugins in markdown.lua

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "jamessan/vim-gnupg",
-    lazy = false,
-    ft = "markdown",
-  },
-  {
-    "lukas-reineke/virt-column.nvim",
-    config = true,
-    event = "BufReadPre",
-    ft = { "markdown" },
-  },
-  {
     "ellisonleao/glow.nvim",
     config = true,
     cmd = "Glow",
