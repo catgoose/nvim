df49f616e93d57f229b729f96049058857b1fe3e
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-06-28 06:14:14 -0500

    Deleted highlighs.scm file.

--- after/queries/vue/highlights.scm
+++
@@ -1,5 +0,0 @@
-; ;; extends
-
-;     ((attribute
-;       (attribute_name) @att_name (#eq? @att_name "class")
-;       (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -16,4 +16,13 @@ return {
     cmd = "Glow",
     ft = { "markdown" },
   },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    ft = { "markdown" },
+    config = true,
+  },
 }
