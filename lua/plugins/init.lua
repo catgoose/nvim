bececc6fe90c4a8fa5b3e4ac8f56949fa949bc05
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-07-03 04:54:55 -0500

    refactor: Update plugin configuration in init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,33 +31,6 @@ return {
     "folke/neoconf.nvim",
     lazy = true,
   },
-  -- {
-  --   "ray-x/go.nvim",
-  --   dependencies = {
-  --     "ray-x/guihua.lua",
-  --     "neovim/nvim-lspconfig",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   config = function()
-  --     require("go").setup()
-  --   end,
-  --   event = { "CmdlineEnter" },
-  --   ft = { "go", "gomod" },
-  --   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  -- },
-  {
-    "olexsmir/gopher.nvim",
-    ft = "go",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "mfussenegger/nvim-dap",
-    },
-    build = function()
-      vim.cmd.GoInstallDeps()
-    end,
-    config = true,
-  },
   {
     "leoluz/nvim-dap-go",
     config = true,
