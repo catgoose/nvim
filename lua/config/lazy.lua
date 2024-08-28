a531c3bed48961e0496c52f9def6ce2ca68c24e4
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-08-28 07:21:01 -0500

    refactor: update lazy.lua configuration for plugin checker with enabled option.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,9 @@ require("lazy").setup("plugins", {
   install = {
     colorscheme = { "kanagawa" },
   },
+  checker = {
+    enabled = true,
+  },
   performance = {
     rtp = {
       disabled_plugins = {
