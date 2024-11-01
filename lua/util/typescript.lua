bfd18a833155ffcb261fdb1e38d4b79c3d63ff0d
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-11-01 08:46:38 -0500

    refactor(typescript.lua): update 'onlies' variable to 'actions' in line 1 and rename 'only' to 'action' throughout the codebase

--- lua/util/typescript.lua
+++ lua/util/typescript.lua
@@ -9,23 +9,23 @@ function M.actions(bufnr)
     return
   end
 
-  local onlies = {
+  local actions = {
     "source.addMissingImports.ts",
     "source.sortImports.ts",
   }
 
-  for _, only in ipairs(onlies) do
+  for _, action in ipairs(actions) do
     local params = vim.lsp.util.make_range_params()
-    params.context = { only = { only } }
+    params.context = { only = { action } }
     local result = vim.lsp.buf_request_sync(bufnr, "textDocument/codeAction", params, 1000)
     if result then
       for _, res in pairs(result) do
         if res.result then
-          for _, action in ipairs(res.result) do
-            if action.edit then
-              vim.lsp.util.apply_workspace_edit(action.edit, "utf-8")
+          for _, r in ipairs(res.result) do
+            if r.edit then
+              vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
             else
-              vim.lsp.buf.execute_command(action.command)
+              vim.lsp.buf.execute_command(r.command)
             end
           end
         end
