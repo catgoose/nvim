cfec40ce1636086c298dc587f7516d248f8f83e5
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2025-02-01 05:22:36 -0600

    refactor: Update LSP servers configuration in servers.lua file.

--- lua/config/lsp/servers.lua
+++ lua/config/lsp/servers.lua
@@ -27,7 +27,7 @@ vim.diagnostic.config({
   -- },
   -- https://github.com/neovim/neovim/pull/31959
   -- https://github.com/neovim/neovim/pull/32187
-  virtual_lines = true,
+  -- virtual_lines = true,
   update_in_insert = false,
   underline = true,
   severity_sort = true,
