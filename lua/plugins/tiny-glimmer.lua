4dd7900da6ba520858c3b5dc73c4559961537bcf
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2025-02-14 05:43:52 -0600

    feat: Remove lua/plugins/highlight-undo.lua and its content

---
+++ lua/plugins/tiny-glimmer.lua
@@ -0,0 +1,52 @@
+local opts = {
+  overwrite = {
+    undo = {
+      enabled = true,
+      default_animation = {
+        name = "fade",
+        settings = {
+          from_color = "DiffDelete",
+          max_duration = 750,
+          min_duration = 500,
+        },
+      },
+      undo_mapping = "u",
+    },
+    redo = {
+      enabled = true,
+      default_animation = {
+        name = "fade",
+        settings = {
+          from_color = "DiffAdd",
+          max_duration = 750,
+          min_duration = 500,
+        },
+      },
+      redo_mapping = "<c-r>",
+    },
+    paste = {
+      enabled = true,
+      default_animation = {
+        name = "fade",
+        settings = {
+          max_duration = 750,
+          min_duration = 500,
+        },
+      },
+      paste_mapping = {
+        lhs = "p",
+        rhs = "<Plug>(YankyPutAfter)",
+      },
+      Paste_mapping = {
+        lhs = "P",
+        rhs = "<Plug>(YankyPutBefore)",
+      },
+    },
+  },
+}
+
+return {
+  "rachartier/tiny-glimmer.nvim",
+  event = "VeryLazy",
+  opts = opts,
+}
--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -8,8 +8,8 @@ local opts = {
     sync_with_ring = true,
   },
   highlight = {
-    on_put = true,
-    on_yank = true,
+    on_put = false,
+    on_yank = false,
     timer = 500,
   },
   preserve_cursor_position = {
@@ -22,8 +22,8 @@ return {
   opts = opts,
   keys = {
     { "y", "<Plug>(YankyYank)", mode = { "n", "x" } },
-    { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
-    { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
+    -- { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" } },
+    -- { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" } },
     { "<c-p>", "<Plug>(YankyCycleForward)" },
     { "<c-n>", "<Plug>(YankyCycleBackward)" },
     { "<leader>pp", "<cmd>YankyRingHistory<cr>" },
