5b16a44cfc2b466be409397216d5073b2c6b046d
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2025-02-17 08:03:31 -0600

    refactor: update plugins initialization configuration in init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,25 +26,4 @@ return {
     ft = "cs",
     config = true,
   },
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      bigfile = { enabled = true },
-      input = { enabled = true },
-      ---
-      dashboard = { enabled = false },
-      explorer = { enabled = false },
-      indent = { enabled = false },
-      notifier = { enabled = false },
-      picker = { enabled = false },
-      quickfile = { enabled = false },
-      scope = { enabled = false },
-      scroll = { enabled = false },
-      statuscolumn = { enabled = false },
-      words = { enabled = false },
-    },
-    enabled = true,
-  },
 }
