8b0485fb2ca5a54130fd49ba61934e2e1af057da
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2025-09-10 15:34:46 -0500

    refactor: Updated tailwind.lua configurations and plugins.

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -2,8 +2,8 @@ local keys = require("util.project").get_keys("twvalues")
 
 return {
   {
-    -- "MaximilianLloyd/tw-values.nvim",
-    dir = "~/git/tw-values.nvim",
+    "MaximilianLloyd/tw-values.nvim",
+    -- dir = "~/git/tw-values.nvim",
     opts = {
       show_unknown_classes = true,
       focus_preview = true,
