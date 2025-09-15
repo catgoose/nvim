4b96e1904fd1a05dacae5c5c29acd09ff2a4363c
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2025-09-15 11:21:41 -0500

    feat: Updated nvim-lint.lua with new golangcilint linter configuration.

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,7 +1,7 @@
 return {
   {
-    "mfussenegger/nvim-lint",
-    -- dir = "~/git/nvim-lint",
+    -- "mfussenegger/nvim-lint",
+    dir = "~/git/nvim-lint",
     event = "VeryLazy",
     lazy = true,
     config = function()
@@ -17,60 +17,13 @@ return {
         editorconfig = { "editorconfig-checker" },
         sh = { "shellcheck" },
         bash = { "shellcheck" },
-        go = { "golangcilint", "fieldalignment", "staticcheck" },
+        go = { "golangcilint", "fieldalignment" },
       }
 
       lint.linters.codespell.args = { "--ignore-words ~/.config/codespell/ignore_words" }
 
       local ignore_buftype = {
         markdown = { "nofile" },
-      }
-
-      lint.linters.fieldalignment = {
-        name = "fieldalignment",
-        cmd = "fieldalignment",
-        args = { "-json" },
-        stdin = false,
-        stream = "stdout",
-        ignore_exitcode = true,
-        parser = function(output, bufnr)
-          if output == "" then
-            return {}
-          end
-          local decoded = vim.json.decode(output, { luanil = { object = true, array = true } })
-          local diagnostics = {}
-          for _, issues in pairs(decoded) do
-            for _, issue_list in pairs(issues) do
-              for _, issue in ipairs(issue_list) do
-                local pos = issue.posn
-                local _, lnum, col = pos:match("^(.+):(%d+):(%d+)$")
-                lnum = tonumber(lnum) or 1
-                col = tonumber(col) or 1
-                local message = issue.message
-                local suggested_fix = ""
-                if issue.suggested_fixes and #issue.suggested_fixes > 0 then
-                  local fix = issue.suggested_fixes[1]
-                  if fix.edits and #fix.edits > 0 then
-                    suggested_fix = fix.edits[1].new
-                    suggested_fix = suggested_fix:gsub("\n", "\n\t"):gsub("\t", "  ")
-                    message = message .. "\nSuggested struct:\n" .. suggested_fix
-                  end
-                end
-                table.insert(diagnostics, {
-                  bufnr = bufnr,
-                  lnum = lnum - 1,
-                  col = col - 1,
-                  end_lnum = lnum - 1,
-                  end_col = col - 1,
-                  severity = vim.diagnostic.severity.WARN,
-                  message = message,
-                  source = "fieldalignment",
-                })
-              end
-            end
-          end
-          return diagnostics
-        end,
       }
 
       -- TODO: 2025-07-31 - Do we need to do this?
