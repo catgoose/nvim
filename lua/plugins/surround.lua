4ce0b6de190787d0387c8471c1c4108814964d08
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2026-02-23 08:17:11 -0600

    refactor: Update keymaps for surround plugin in lua/plugins/surround.lua

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,16 +1,4 @@
 local opts = {
-  keymaps = {
-    insert = "<C-g>s",
-    insert_line = "<C-g>S",
-    normal = "ys",
-    normal_cur = "yss",
-    normal_line = "yS",
-    normal_cur_line = "ySS",
-    visual = "S",
-    visual_line = "gS",
-    delete = "ds",
-    change = "cs",
-  },
   aliases = {
     ["a"] = "<",
     ["b"] = "(",
